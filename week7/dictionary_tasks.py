4a6c094f6873f46d0b76771bf359e39bd839e864
Callum Boylan <2boylc17@solent.ac.uk>
2023-11-09 12:45:12 +0000

    yes

---
+++ week7/dictionary_tasks.py
@@ -0,0 +1,27 @@
+def pattern():
+    sequences = {"Short Sequence": 3, "Medium Sequence": 2, "Long Sequence": 1}
+    return sequences
+
+
+def display_keys(data):
+    for key in data:
+        print(key)
+
+
+def display_values(data):
+    for value in data.values():
+        print(value)
+
+
+def display_items(data):
+    for item in data.items():
+        print(item)
+
+
+def run():
+    display_keys(pattern())
+    display_values(pattern())
+    display_items(pattern())
+
+
+run()
